25991a1f33a288a3c64b57056a1c63367267a110
Justin Bishop <jubishop@gmail.com>
2021-05-06 23:54:21 -0700

    move over apparition context

--- lib/tony/rspec/context.rb
+++ lib/tony/rspec/context.rb
@@ -2,6 +2,20 @@ require 'capybara/rspec'
 require 'rack/test'
 require 'rspec'
 
+RSpec.shared_context(:tony_apparition) {
+  include Capybara::RSpecMatchers
+  include Tony::Test::Apparition::Cookies
+
+  before(:each) {
+    page.driver.headers = { Origin: 'http://localhost' }
+  }
+
+  after(:each) {
+    clear_cookies
+    Capybara.reset_sessions!
+  }
+}
+
 RSpec.shared_context(:tony_rack_test) {
   include Capybara::RSpecMatchers
   include Rack::Test::Methods
