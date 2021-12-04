07358fa7795fbcb8bdf7516d26816864785e46f1
Justin Bishop <jubishop@gmail.com>
2021-12-04 12:12:42 -0800

    bump gem with new save_screenshot call

--- lib/tony/goldens/page.rb
+++ lib/tony/goldens/page.rb
@@ -21,7 +21,7 @@ module Tony
 
           expect(@page).to(have_googlefonts)
 
-          @page.driver.save_screenshot(tmp_file(filename), { full: true })
+          @page.driver.save_screenshot(tmp_file(filename), full: true)
 
           unless File.exist?(golden_file(filename))
             warn("Golden not found for: #{filename}".red)
