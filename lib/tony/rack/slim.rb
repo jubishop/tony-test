a2eda5da30cdc3a2fc5f4dce393f06fc33f54156
Justin Bishop <jubishop@gmail.com>
2021-12-28 13:35:23 -0800

    call original slim.render in the mocked version

--- lib/tony/rack/slim.rb
+++ lib/tony/rack/slim.rb
@@ -5,10 +5,12 @@ module Tony
     module Rack
       module Slim
         def expect_slim(template, views: nil, layout: nil, **attributes)
+          original_render = Tony::Slim.instance_method(:render)
           expect_any_instance_of(Tony::Slim).to(receive(:render)).with(
               template, **attributes).exactly(1).time { |slim|
                 expect(slim.views).to(eq(views)) if views
                 expect(slim.layout.file.chomp('.slim')).to(eq(layout)) if layout
+                original_render.bind_call(slim, template, **attributes)
               }
         end
       end
--- tony-test.gemspec
+++ tony-test.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |spec|
   spec.name          = 'tony-test'
-  spec.version       = '0.61'
+  spec.version       = '0.62'
   spec.summary       = %q(Helpers for testing Tony.)
   spec.authors       = ['Justin Bishop']
   spec.email         = ['jubishop@gmail.com']
