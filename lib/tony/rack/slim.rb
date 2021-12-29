556c97146d3e7edea5fc618e5771ae42d5708577
Justin Bishop <jubishop@gmail.com>
2021-12-28 16:30:15 -0800

    pass true args to slim render expectation

--- lib/tony/rack/slim.rb
+++ lib/tony/rack/slim.rb
@@ -7,10 +7,10 @@ module Tony
         def expect_slim(template, views: nil, layout: nil, **attributes)
           original_render = Tony::Slim.instance_method(:render)
           expect_any_instance_of(Tony::Slim).to(receive(:render)).with(
-              template, **attributes).exactly(1).time { |slim|
+              template, **attributes).exactly(1).time { |slim, *args|
                 expect(slim.views).to(eq(views)) if views
                 expect(slim.layout.file.chomp('.slim')).to(eq(layout)) if layout
-                original_render.bind_call(slim, template, **attributes)
+                original_render.bind_call(slim, *args)
               }
         end
       end
